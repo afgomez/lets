6936422a2743b86ca3e6c626f421a2c039274850
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2020-04-13 11:26:57 +0200

    Parse action from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+const USAGE: &'static str = "
+lets - A simple distraction blocker
+
+Blocks hosts specified on a file. It needs write access to /etc/hosts
+
+Usage:
+    $ sudo lets play
+    $ sudo lets work
+
+Configuration:
+    To block hosts, edit `/etc/hosts.block` and add one host per line.
+";
+
 fn main() {
-    println!("Hello, world!");
+    let arg = std::env::args().nth(1);
+    let action = arg.unwrap_or(String::from(""));
+
+    match action.as_str() {
+        "play" => println!("Let's play!"),
+        "work" => println!("Let's work!"),
+        _ => eprintln!("{}", &USAGE),
+    }
 }
